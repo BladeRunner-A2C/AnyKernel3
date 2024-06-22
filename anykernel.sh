653651586b2de6b49032e7106a2cf4ab08898bf7
BladeRunner-A2C <john.smith@unused.email>
2024-06-22 14:27:57 +0600

    AK3: Add suport for pdx215

--- anykernel.sh
+++ anykernel.sh
@@ -4,21 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=QuicksilveR Kernel by ghostrider-reborn
+kernel.string=QuicksilveR Kernel by Impala#67
 do.devicecheck=1
-do.modules=0
+do.modules=1
 do.systemless=1
+device.name1=pdx215
+device.name2=pdx214
+device.name3=sagami
+device.name4=XQ-BC72
+device.name5=XQ-BC52
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=
-device.name2=
 '; } # end properties
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+block=boot;
+is_slot_device=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
@@ -26,31 +25,21 @@ patch_vbmeta_flag=auto;
 . tools/ak3-core.sh;
 
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel boot install
-dump_boot;
-write_boot;
+split_boot;
+flash_boot;
 ## end boot install
 
 
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-#patch_vbmeta_flag=auto;
+# vendor_boot shell variables
+block=vendor_boot;
+is_slot_device=auto;
+
 
 # reset for vendor_boot patching
-#reset_ak;
-
+reset_ak;
 
 ## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
+split_boot;
+flash_boot;
 ## end vendor_boot install
-
